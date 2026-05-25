3d8d5b5bf8e59d851c690ab7e3c56a24c3c26568
tfshorifulislam <tfshorifulislam@gmail.com>
2026-05-26 01:48:51 +0600

    next config js errro solve

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,20 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
