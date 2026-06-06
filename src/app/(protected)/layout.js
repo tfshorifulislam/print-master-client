80cd72e23df5798e1155de6d57da8932aae1b475
tfshorifulislam <tfshorifulislam@gmail.com>
2026-06-07 04:51:44 +0600

    protected layout update

--- src/app/(protected)/layout.js
+++ src/app/(protected)/layout.js
@@ -1,31 +1,17 @@
-import { Geist, Geist_Mono, Inter } from "next/font/google";
-import "./globals.css";
-import { Providers } from "../providers";
 import Asidebar from "@/components/Asidebar";
-import RightSideBar from "@/components/RightSideBar";
 import MobileScreenBottomNavbar from "@/components/MobileNavbar";
+import RightSideBar from "@/components/RightSideBar";
 
-
-export default function RootLayout({ children }) {
+export default function ProtectedLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`h-full antialiased`}
-      suppressHydrationWarning
-    >
-      <body className="min-h-screen bg-background text-foreground">
-        <Providers>
-          <div className="flex max-w-350 mx-auto">
-            <Asidebar />
-            <main
-              className="flex-1 min-h-screen">
-              {children}
-            </main>
-            <MobileScreenBottomNavbar />
-            <RightSideBar />
-          </div>
-        </Providers>
-      </body>
-    </html>
+    <div className="flex max-w-350 mx-auto">
+      <Asidebar />
+      <main
+        className="flex-1 min-h-screen">
+        {children}
+      </main>
+      <MobileScreenBottomNavbar />
+      <RightSideBar />
+    </div>
   );
 }
